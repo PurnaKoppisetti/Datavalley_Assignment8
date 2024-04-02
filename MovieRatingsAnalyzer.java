6c64f2a790c9a3396c14d94fb3fbe82f4f6e22fb
PurnaKoppisetti <163108322+PurnaKoppisetti@users.noreply.github.com>
2024-04-02 17:34:45 +0530

    Update MovieRatingsAnalyzer.java

--- MovieRatingsAnalyzer.java
+++ MovieRatingsAnalyzer.java
@@ -29,4 +29,26 @@ public class MovieRatingsAnalyzer {
         movies.add(new Movie("Movie4", "PG-13", 4.0));
         movies.add(new Movie("Movie5", "PG", 3.9));
 
-   …
+        Map<String, Integer> ratingCounts = new HashMap<>();
+        Map<String, Double> ratingAverages = new HashMap<>();
+
+        for (Movie movie : movies) {
+            String rating = movie.getRating();
+            double score = movie.getScore();
+
+            if (!ratingCounts.containsKey(rating)) {
+                ratingCounts.put(rating, 1);
+                ratingAverages.put(rating, score);
+            } else {
+                ratingCounts.put(rating, ratingCounts.get(rating) + 1);
+                ratingAverages.put(rating, ratingAverages.get(rating) + score);
+            }
+        }
+
+        for (String rating : ratingCounts.keySet()) {
+            int count = ratingCounts.get(rating);
+            double average = ratingAverages.get(rating) / count;
+            System.out.println("Rating: " + rating + ", Number of Movies: " + count + ", Average Rating: " + average);
+        }
+    }
+}
